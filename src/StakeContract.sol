ffbbe3bb534d79cea60d82ffa7dc769e356d373a
mooezz999 <moustafaezz999@gmail.com>
2022-09-14 19:12:34 +0200

    mm

---
+++ src/StakeContract.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.8.13;
+
+import "@Openzeppelin/contracts/token/ERC20/IERC20.sol";
+error StakeContract__TransferFailed();
+
+contract StakeContract {
+    mapping(address => uint256) public s_balances;
+
+    function stake(uint256 amount, address token) external returns (bool) {
+        s_balances[msg.sender] += amount;
+        bool success = IERC20(token).transferFrom(
+            msg.sender,
+            address(this),
+            amount
+        );
+        if (!success) revert StakeContract__TransferFailed();
+        return success;
+    }
+}
